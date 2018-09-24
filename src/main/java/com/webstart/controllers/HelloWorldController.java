8203699d686ff8be8599db61877b22c59f81911a
Garrisnix <15367876+Garrisonix@users.noreply.github.com>
2018-09-24 03:44:28 -0700

    Helloworld

---
+++ src/main/java/com/webstart/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.webstart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String Hello(){
+        return "index";
+    }
+}
