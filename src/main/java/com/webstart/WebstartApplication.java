77589e1e004ab5365708e7071e771a14b816f25d
Garrisnix <15367876+Garrisonix@users.noreply.github.com>
2018-09-24 03:29:58 -0700

    initial commit

---
+++ src/main/java/com/webstart/WebstartApplication.java
@@ -0,0 +1,12 @@
+package com.webstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebstartApplication.class, args);
+    }
+}
