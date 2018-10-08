9a5d3f8e7eddb63b7191c24157e1e4957bbd027a
Garrisnix <15367876+Garrisonix@users.noreply.github.com>
2018-10-07 23:57:14 -0700

    logging and Navbar

--- src/main/java/com/webstart/web/i18n/I18nService.java
+++ src/main/java/com/webstart/web/i18n/I18nService.java
@@ -1,5 +1,7 @@
 package com.webstart.web.i18n;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -12,11 +14,15 @@ public class I18nService {
     @Autowired
     private MessageSource messageSource;
 
+    /** Application Logger */
+    public static final Logger LOG = LoggerFactory.getLogger(I18nService.class);
+
     /**
      * Returns a message for the given string and the default locale in the session context
      * @param id The key to the message resource file
      */
     public String getMessage(String id){
+        LOG.info("Returning I18n text for messageId {}", id);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(id, locale);
     }
