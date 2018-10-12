88213eb3ac80c5833d0ffb7d5b7c8e908ac295d6
Garrisnix <15367876+Garrisonix@users.noreply.github.com>
2018-10-11 23:50:45 -0700

    Feedback 1.0

---
+++ src/main/java/com/webstart/web/controllers/ContactController.java
@@ -0,0 +1,33 @@
+package com.webstart.web.controllers;
+
+import com.webstart.web.domain.frontend.FeedbackPojo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ContactController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ContactController.class);
+
+    public static final String FEEDBACK_MODEL_KEY = "feedback";
+
+    private static final String CONTACT_US_VIEW_MODE = "contact/contact";
+
+    @RequestMapping(value = "/contact", method = RequestMethod.GET)
+    public String contactUsGET(ModelMap model){
+        FeedbackPojo feedbackPojo = new FeedbackPojo();
+        model.addAttribute(ContactController.FEEDBACK_MODEL_KEY, feedbackPojo);
+        return ContactController.CONTACT_US_VIEW_MODE;
+    }
+
+    @RequestMapping(value = "/contact",method = RequestMethod.POST)
+    public String contactUsPOST(@ModelAttribute(FEEDBACK_MODEL_KEY) FeedbackPojo feedback){
+        LOG.debug("Feedback POJO content: {}", feedback);
+        return ContactController.CONTACT_US_VIEW_MODE;
+    }
+}
