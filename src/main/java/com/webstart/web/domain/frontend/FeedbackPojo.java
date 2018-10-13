12356a035ec0974e0f4f0a0f4e201794ade30d86
Garrisnix <15367876+Garrisonix@users.noreply.github.com>
2018-10-12 20:19:17 -0800

    Feedback fix

--- src/main/java/com/webstart/web/domain/frontend/FeedbackPojo.java
+++ src/main/java/com/webstart/web/domain/frontend/FeedbackPojo.java
@@ -2,14 +2,25 @@ package com.webstart.web.domain.frontend;
 
 import java.io.Serializable;
 
+/**
+ * Created by tedonema on 20/03/2016.
+ */
 public class FeedbackPojo implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private String email;
     private String firstName;
     private String lastName;
-    private String email;
     private String feedback;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -27,28 +38,20 @@ public class FeedbackPojo implements Serializable {
         this.lastName = lastName;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getFeedback() {
         return feedback;
     }
 
-    public void setFeedBack(String feedback) {
+    public void setFeedback(String feedback) {
         this.feedback = feedback;
     }
 
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("FeedbackPojo{");
-        sb.append("firstName='").append(firstName).append('\'');
+        sb.append("email='").append(email).append('\'');
+        sb.append(", firstName='").append(firstName).append('\'');
         sb.append(", lastName='").append(lastName).append('\'');
-        sb.append(", email='").append(email).append('\'');
         sb.append(", feedback='").append(feedback).append('\'');
         sb.append('}');
         return sb.toString();
