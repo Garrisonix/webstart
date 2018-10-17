e773a3d5d77a75647903381501b659d610f65048
Garrisnix <15367876+Garrisonix@users.noreply.github.com>
2018-10-16 17:00:24 -0700

    Pom update

--- src/main/java/com/webstart/web/controllers/LoginController.java
+++ src/main/java/com/webstart/web/controllers/LoginController.java
@@ -1,4 +1,15 @@
 package com.webstart.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LoginController {
+    public static final String LOGIN_VIEW_NAME = "user/login";
+
+    @RequestMapping("/login")
+    public String login(){
+        return LOGIN_VIEW_NAME;
+    }
+
 }
